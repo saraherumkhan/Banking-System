93c4488076182a65765d6d7778b015df38cdc2ee
Sarah Khan <sairasek1212@gmail.com>
2021-05-18 18:35:39 +0530

    add banking system project

---
+++ navbar.php
@@ -0,0 +1,20 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #094261;"><b>H U M A N I T Y $ B A N K</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #000000;"><b>HOME</b></a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #000000;"><b>VIEW CUSTOMER</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #000000;"><b>TRANSACTION HISTORY</b></a>
+              </li>
+          </div>
+       </nav>
